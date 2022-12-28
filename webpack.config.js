4c6d1411d979968fe8db82e616fda97721fc7981
Sagnik Chakraborty <sagnik.chakraborty910@yahoo.com>
2022-12-29 02:01:49 +0530

    added babel and its setup/loader

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,16 @@ module.exports = {
         test: /\.scss$/i,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
     ],
   },
   plugins: [
