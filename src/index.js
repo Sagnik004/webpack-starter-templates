4569e9349ae28db974c53c5268016add5249186a
Sagnik Chakraborty <sagnik.chakraborty910@yahoo.com>
2022-12-29 02:20:48 +0530

    asset resource loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import generateJoke from "./generateJoke";
+import generateJoke from './generateJoke';
 import './styles/main.scss';
+import laughSvg from './assets/laughing.svg';
+
+const laughImg = document.getElementById('laughImg');
+laughImg.src = laughSvg;
 
 console.log(generateJoke());
